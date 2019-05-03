b66f4eb5dfff224623761df224cb0331278d7720
luanbrito7 <lab7@cin.ufpe.br>
2019-05-02 21:18:43 -0300

    add basic upload img to firebase

---
+++ api/firebase/upload.js
@@ -0,0 +1,33 @@
+const uuid = require('uuid/v4')
+const firebase = require('firebase')
+const storage = firebase.storage()
+
+let firebaseConn = {}
+
+firebaseConn.IMAGE = 'IMAGE'
+
+firebaseConn.upload = async (uri, type, name = null) => {
+    let folder = " "
+    folder = type == firebaseConn.IMAGE ?
+        firebaseConn.IMAGE.toLocaleLowerCase()
+        : 'default'
+
+    let fileName = name
+    if (fileName == null) {
+        fileName = await uuid()
+    }
+
+    console.log(fileName)
+    
+    const firebasePath = `${folder}/${fileName}`
+    const imageRef = await storage.ref(firebasePath).put(uri)
+
+    return {
+        url: imageRef.downloadURL,
+        fileName,
+        type,
+        firebasePath
+    }
+}
+
+module.exports = firebaseConn
